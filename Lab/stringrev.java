bff9e174311178534bbece6fe8382a193fda7d9a
devaprasad-n-m <59179637+devaprasad-n-m@users.noreply.github.com>
2021-10-01 12:22:22 +0530

    Update stringrev.java

--- Lab/stringrev.java
+++ Lab/stringrev.java
@@ -1,51 +1,250 @@
 import java.io.*;
 
-class stringrev {
-    int n;
-    int sp;
-    char[] a;
+import java.io.*;
 
-    stringrev(int nn) {
-        sp = -1;
-        n = nn;
-        a = new char[n];
+//link
+class Link {
+    public int data;
+    public Link next;
+
+    public Link(int d) {
+        data = d;
+        this.next = null;
     }
 
-    boolean push(char x) {
-        if (sp >= n) {
-            System.out.println("Stack Overflow");
-            return false;
+    public void displayLink() {
+        System.out.println(data);
+    }
+
+}
+
+// Linked List
+class LinkList {
+    private Link first;
+
+    public LinkList() {
+        first = null;
+    }
+
+    public boolean isEmpty() {
+        return (first == null);
+    }
+
+    // insert the link to the first position;
+    public void insertFirst(int d) {
+        Link nl = new Link(d);
+        nl.next = first;
+        first = nl;
+    }
+
+    // insert the link to the last posion
+    public void insertLast(int d) {
+        Link nl = new Link(d);
+        Link cur = first;
+        if (isEmpty()) {
+            first = nl;
+            nl.next = null;
         } else {
-            a[++sp] = x;
-            return true;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = nl;
+            nl.next = null;
         }
     }
 
-    char pop() {
-        if (sp < 0) {
-            System.out.println("Stack Underflow");
-            return 0;
-        } else {
-            char x = a[sp--];
-            return x;
+    // Insert at the position of a given value.
+
+    public void insert(int key, int d) {
+        Link cur = first;
+        Link prev = first;
+        Link newLink = new Link(d);
+
+        while (cur.data != key) {
+            if (cur.next == null) {
+                System.out.println("Key not Found");
+                break;
+            } else {
+                prev = cur;
+                cur = cur.next;
+            }
+
+        }
+        newLink.next = prev.next;
+        prev.next = newLink;
+
+    }
+
+    // delete the first element
+
+    public int deleteFirst() {
+        int temp = first.data;
+        // if list is Empty
+        if (isEmpty()) {
+            System.out.println("List is Empty");
+        }
+        first = first.next;
+        return temp;
+
+    }
+    // delete the last element
+
+    public int deleteLast() {
+        Link cur = first;
+        Link prev = first;
+        while (cur.next != null) {
+            prev = cur;
+            cur = cur.next;
+        }
+        int temp = cur.data;
+        prev.next = null;
+        return temp;
+    }
+
+    // delete link containing a key
+
+    public void delete(int key) {
+        Link cur = first;
+        Link prev = first;
+
+        while (cur.data != key) {
+            if (cur.next == null) {
+                System.out.println("Key not found");
+                break;
+            }
+
+            else {
+                prev = cur;
+                cur = cur.next;
+            }
+
+        }
+        if (cur == first)
+            first = first.next;
+        else
+            prev.next = cur.next;
+
+    }
+
+    // display all elements
+
+    public void displayList() {
+        Link current = first;
+        // if list is Empty
+        if (isEmpty()) {
+            System.out.println("THE LIST IS EMPTY");
+        }
+        while (current != null) {
+            current.displayLink();
+            current = current.next;
         }
     }
 
+    // Concatanation
+    public void concatenate(LinkList obj) {
+        Link cur1 = first;
+        Link cur2 = obj.first;
+
+        while (cur1.next != null) {
+            cur1 = cur1.next;
+        }
+        cur1.next = cur2;
+
+    }
+
+}
+
+public class q4_b_2k19 {
     public static void main(String args[]) throws IOException {
-        int l, i;
-        String str;
+        LinkList[] obj;
+        obj = new LinkList[2];
+        obj[0] = new LinkList();
+        obj[1] = new LinkList();
+        int choice, value, value1, i;
+
         InputStreamReader read = new InputStreamReader(System.in);
         BufferedReader in = new BufferedReader(read);
-        System.out.println("Enter the String");
-        str = in.readLine();
-        l = str.length();
-        stringrev obj = new stringrev(l);
-        for (i = 0; i < l; i++)
-            obj.push(str.charAt(i));
 
-        for (i = 0; i < l; i++) {
-            char ch = obj.pop();
-            System.out.print(ch);
-        }
+        System.out.println("###**Single Linked List**####");
+        do {
+            System.out.println("Press");
+            System.out.println("0.First LinkList");
+            System.out.println("1.2nd LinkList");
+
+            i = Integer.parseInt(in.readLine());
+
+            do {
+                System.out.println("SINGLE LINKED LIST");
+                System.out.println("1.INSERT FIRST");
+                System.out.println("2.INSERT LAST");
+                System.out.println("3.INSERT LINK");
+                System.out.println("4.DELETE FIRST");
+                System.out.println("5.DELETE LAST");
+                System.out.println("6.DELETE LINK");
+                System.out.println("7.DISPLAY ALL");
+                if(i==1)
+                System.out.println("8.CONCATANATION");
+                System.out.println("Press 9 for completing");
+                choice = Integer.parseInt(in.readLine());
+
+                switch (choice) {
+                case 1: {
+                    System.out.println("Enter the data");
+                    value = Integer.parseInt(in.readLine());
+                    obj[i].insertFirst(value);
+                    break;
+                }
+                case 2: {
+                    System.out.println("Enter the data");
+                    value = Integer.parseInt(in.readLine());
+                    obj[i].insertLast(value);
+                    break;
+                }
+                case 3: {
+                    System.out.println("Enter the data to be added");
+                    value = Integer.parseInt(in.readLine());
+                    System.out.println("Enter the linked list value ");
+                    value1 = Integer.parseInt(in.readLine());
+                    obj[i].insert(value1, value);
+                    break;
+                }
+                case 4: {
+                    int d = obj[i].deleteFirst();
+                    System.out.println(d + " is Deleted");
+                    break;
+                }
+                case 5: {
+                    int d = obj[i].deleteLast();
+                    System.out.println(d + " is Deleted");
+                    break;
+                }
+                case 6: {
+                    System.out.println("Enter the data to be deleted");
+                    value = Integer.parseInt(in.readLine());
+                    obj[i].delete(value);
+                    System.out.println("Deleted the data");
+                    break;
+                }
+                case 7: {
+                    System.out.println("Display all elements in the list");
+                    obj[i].displayList();
+                    break;
+                }
+                case 8: {
+                    obj[0].concatenate(obj[1]);
+                 System.out.println(" CONCATENATED ");
+                 obj[0].displayList();     
+                 break;
+                }
+                default:
+                    System.out.println("Completed");
+
+                }
+
+            } while (choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 5 || choice == 6
+                    || choice == 7||choice==8);
+
+        } while (i == 0 || i == 1);
+
     }
 }
